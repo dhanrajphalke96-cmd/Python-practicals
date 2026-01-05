e63e0cd872ef8035e9c4dadf5a5ec7d92aa4c5a6
Dhanraj-96 <dhanrajphalke96@gmail.com>
2026-01-05 14:39:25 +0530

    number guessing game

---
+++ practical_no1.py
@@ -0,0 +1,29 @@
+import random 
+
+computer = random.randint(1, 100)
+no_of_guesses = 0
+
+
+while True:
+
+    user = int(input("Enter a number between 1 and 100: "))
+    no_of_guesses += 1
+    if no_of_guesses > 10:
+        print("Sorry, you've used all your guesses. The correct number was", computer)
+        break
+
+    if user < 1 or user > 100:
+        print("Invalid input. Please enter a number between 1 and 100.")
+        continue
+
+    if computer == user:
+        print("Congratulations! You guessed the correct number.")
+        print("Number of guesses taken:", no_of_guesses)
+        break
+        
+    elif computer > user:
+        print("your guess is too low ")
+
+    else:
+        print("your guess is too high ")
+        
