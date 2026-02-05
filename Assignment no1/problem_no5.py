c93a0b9f8bcf0f3d95c57a2e7ff0bbdd9bc81aff
Dhanraj-96 <dhanrajphalke96@gmail.com>
2026-02-05 14:18:51 +0530

    Number swapping

---
+++ Assignment no1/problem_no5.py
@@ -0,0 +1,4 @@
+x=10+3*2**2
+print(x)
+
+#The ** comes frist in the order then * and the +
---
+++ Assignment no1/problem_no6.py
@@ -0,0 +1,6 @@
+a=int(input("Enter a no :"))
+b = int(input("Enter a no :"))
+c = a
+a = b
+b = c
+print(f"a= {a} & b = {b}")
