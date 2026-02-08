3b8132a0d56dc01f8d754377ea2e87324abf1755
Dhanraj-96 <dhanrajphalke96@gmail.com>
2026-02-08 17:34:25 +0530

    Simple Interest

---
+++ Assignment no1/problem_no9.py
@@ -0,0 +1,7 @@
+P = float(input("Enter Principal (P): "))
+R = float(input("Enter Rate of Interest (R): "))
+T = float(input("Enter Time (T): "))
+
+SI = (P * R * T) / 100
+
+print("Simple Interest =", SI)
