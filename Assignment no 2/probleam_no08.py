2f9c1379d591bc56568555dac8b3edff4ac85f76
Dhanraj-96 <dhanrajphalke96@gmail.com>
2026-02-13 07:31:53 +0530

    mini calculetor

---
+++ Assignment no 2/probleam_no08.py
@@ -0,0 +1,18 @@
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : "))
+c = input("Enter a opretor :+ , - , * , / :")
+
+if c == '+':
+    print(a+b)
+
+elif c == '-':
+    print(a-b)
+
+elif c == '*':
+    print(a*b)
+
+elif c =='/':
+    print(a/b) 
+
+else :
+    print("Enter a valid opretor ")
